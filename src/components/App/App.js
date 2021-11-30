33061de2523efa025b85f947380a25df5104718f
Jessen Mercado <jessen.mercado@metafinanciera.com>
2021-11-30 08:55:14 -0600

    Persistencia de datos con LolcalStorage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,18 +4,26 @@ import { TodoSearch } from '../TodoSearch/TodoSearch'
 import { TodoList } from '../TodoList/TodoList'
 import { TodoItem } from '../TodoItem/TodoItem'
 import { CreateTodoButton } from '../CreateTodoButton/CreateTodoButton'
-// import './App.css';
+import './App.css'
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: false },
-]
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+//   { text: 'LALALALAA', completed: false },
+// ]
 
 function App() {
+  const localStorageTodos = localStorage.getItem('TODOS_V1')
+  let parsedTodos
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+    parsedTodos = []
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
   const [searchValue, setSearchValue] = useState('')
-  const [todos, setTodos] = useState(defaultTodos)
+  const [todos, setTodos] = useState(parsedTodos)
 
   //Cantidad de todos marcados como completados
   const completedTodos = todos.filter((todo) => todo.completed === true).length
@@ -34,6 +42,12 @@ function App() {
     })
   }
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos)
+    localStorage.setItem('TODOS_V1', stringifiedTodos)
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
     //Cada vez que reciba un texto va a buscar cual todo cumple con esa condicion
     const todoIndex = todos.findIndex((todo) => todo.text === text)
@@ -42,14 +56,14 @@ function App() {
     //Marcamos a ese todo como caso contrario a lo que tenga
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed
     //Actualizamos el estado para re-renderizar
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text)
     const newTodos = [...todos]
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   return (
